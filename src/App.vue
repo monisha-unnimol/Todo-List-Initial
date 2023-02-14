40ffbb29fde880922e0a853d3d6b7fd2f631fa33
monisha-kv <monisha@keyvalue.systems>
2023-02-14 17:12:27 +0530

    style fix

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,33 @@
 </script>
 
 <template>
-  <div>
+  <h1>
     Todo List
-  </div>
+  </h1>
 </template>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Itim:wght@300;400&display=swap');
 
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Itim', 'sans-serif';
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
 </style>
