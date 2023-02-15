fef13b4438bc3f348b6ea49b6773a046998f8353
Meenu Iswar <meenu@keyvalue.systems>
2023-02-15 14:45:17 +0530

    Adding getters in store

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,52 +1,76 @@
 <script setup lang="ts">
-import { computed, ref, watch, watchEffect } from 'vue';
+import { computed, ref, watch, watchEffect } from "vue";
 
-import Header from './Header.vue';
-import TaskItem from './TaskItem.vue';
-import TaskForm from './TaskForm.vue';
+import Header from "./Header.vue";
+import TaskItem from "./TaskItem.vue";
+import TaskForm from "./TaskForm.vue";
 
-import type { Task } from '@/types/task';
-import { useTaskStore } from '@/store/tasks';
+import type { Task } from "@/types/task";
+import { useTaskStore } from "@/store/tasks";
 
 const taskStore = useTaskStore();
 const tasks = ref<Task[]>(taskStore.tasks);
-const noOfTasks = computed(() => tasks.value.length)
+const noOfTasks = computed(() => tasks.value.length);
+const showOnlyReminders = ref(false);
 
 const editTaskClick = (task: Task) => {
   taskStore.editTaskDetails = task;
   taskStore.toggleForm();
-}; 
+};
 
-watch(noOfTasks, () => console.log('watch', noOfTasks.value))
+watch(noOfTasks, () => console.log("watch", noOfTasks.value));
 watchEffect(() => {
   if (noOfTasks.value === 0) {
-    taskStore.showForm = true
+    taskStore.showForm = true;
   }
-  console.log('watchEffect', noOfTasks.value)
-})
-watchEffect(() => tasks.value = taskStore.tasks);
-
+  console.log("watchEffect", noOfTasks.value);
+});
+watchEffect(() => (tasks.value = taskStore.tasks));
 </script>
 
 <template>
   <div class="container">
     <Header name="Monisha" />
+    <div class="filter">
+      <input type="checkbox" id="checkbox" v-model="showOnlyReminders" />
+      <label for="checkbox">Show only reminders</label>
+    </div>
     <div v-if="taskStore.showForm">
       <TaskForm
         @create-task="taskStore.addTask"
         @update-task="taskStore.updateTask"
       />
     </div>
-    <TaskItem
-      v-for="task in tasks"
-      :key="task.id"
-      :task="task"
-      @edit="editTaskClick"
-      @delete-task="taskStore.removeTask"
-    />
+    <div v-if="showOnlyReminders">
+      <TaskItem
+        v-for="task in taskStore.reminders"
+        :key="task.id"
+        :task="task"
+        @edit="editTaskClick"
+        @delete-task="taskStore.removeTask"
+      />
+    </div>
+    <div v-else>
+      <TaskItem
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        @edit="editTaskClick"
+        @delete-task="taskStore.removeTask"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.filter {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+label {
+  margin-left: 5px;
+}
 </style>
--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,35 +1,39 @@
-import type { Task } from '@/types/task';
-import { defineStore } from 'pinia';
+import type { Task } from "@/types/task";
+import { defineStore } from "pinia";
 
-export const useTaskStore = defineStore('tasks', {
+export const useTaskStore = defineStore("tasks", {
   state: () => {
     return {
-      tasks: [{ id : '1', title: 'Task 1',  date: '2023-03-18', reminder: false }] as Task[],
+      tasks: [
+        { id: "1", title: "Task 1", date: "2023-03-18", reminder: false },
+      ] as Task[],
       showForm: false,
-      editTaskDetails: {} as Task | null
-    }
+      editTaskDetails: {} as Task | null,
+    };
   },
   getters: {
-    
+    reminders(): Task[] {
+      return this.tasks.filter((task) => task.reminder);
+    },
   },
   actions: {
     toggleForm() {
-      this.showForm = !this.showForm
+      this.showForm = !this.showForm;
       if (this.editTaskDetails?.id && !this.showForm) {
-        this.editTaskDetails = null
+        this.editTaskDetails = null;
       }
     },
     addTask(task: Task) {
-      this.tasks = [...this.tasks, task]
-      this.toggleForm()
+      this.tasks = [...this.tasks, task];
+      this.toggleForm();
     },
     removeTask(id: string) {
-      this.tasks = this.tasks.filter((task) => task.id !== id)
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
     updateTask(newTask: Task) {
-      this.toggleForm()
+      this.toggleForm();
       const index = this.tasks.findIndex((task) => task.id === newTask.id);
       this.tasks[index] = newTask;
-    }
-  }
-})
+    },
+  },
+});
